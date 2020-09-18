4f4f6c00dbe55a53f149b362c589ae0ee70205bf
Desmond <desmondaldridge@gmail.com>
2020-09-18 15:02:22 -0700

    Added all the question prompts

--- Develop/index.js
+++ Develop/index.js
@@ -1,9 +1,60 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("generateMarkdown");
+const Choices = require("inquirer/lib/objects/choices");
 
 
-// array of questions for user
-const questions = [
-
-];
+// Questions
+function promptUser() {
+    return inquirer.prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the title of your project?"
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Please input a brief description of your project:"
+      },
+      {
+        type: "input",
+        name: "installation",
+        message: "Please input your project's installation instructions:"
+      },
+      {
+        type: "input",
+        name: "usage",
+        message: "Please input your project's usage information:"
+      },
+      {
+        type: "input",
+        name: "contribution",
+        message: "Please input your project's contribution guidelines:"
+      },
+      {
+        type: "input",
+        name: "test",
+        message: "Please input your project's test instructions:"
+      },
+      {
+        type: "input",
+        name: "license",
+        message: "Please choose a license for your project:",
+        choices: ["Academic Free License v3.0", "Apache 2.0", "BSD 3", "Creative Commons", "wtfpl", "GPL", "MIT", "None"]
+      },
+      {
+        type: "input",
+        name: "username",
+        message: "What is your GitHub username?"
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is your email address?"
+      },
+    ]);
+  }
 
 // function to write README file
 function writeToFile(fileName, data) {
