03a7b72fa48e3a40197e5f4b9b536e21a12a95f0
Desmond <desmondaldridge@gmail.com>
2020-09-19 20:55:32 -0700

    Working on the generated markdown... the badges are going to be tricky

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,36 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-  ## Description
+
+  ## DESCRIPTION
+  ${data.description}
+
+  ## TABLE OF CONTENTS
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contribution](#contribution)
+  - [Test](#test)
+  - [Questions](#questions)
+
+  ## INSTALLATION
+  ${data.installation}
+
+  ## USAGE
+  ${data.usage}
+
+  ## LICENSE
+  ${data.license}
+
+  ## TEST
+  ${data.test}
+
+  ## CONTRIBUTION
+  ${data.contribution}
+
+  ## QUESTIONS
+
+
 
 `;
 }
